6f884611bdc3a9659692730c0ac0420bb3504430
huzaifaghoury <huzaifaghourydev@gmail.com>
2024-03-29 03:45:09 -0700

    changes

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import LandingPage from './landingpage/page'
+
+ 
+
+const Page = () => {
+  return (
+    <div>
+
+<LandingPage/>
+    
+   
+    </div>
+  );
+};
+
+export default  Page; 
